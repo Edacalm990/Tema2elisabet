88a066a9b3b1a420dcc1fd10365b56d9c222e2aa
Edacalm990 <edacalm990@g.educaand.es>
2022-09-29 10:17:16 +0200

    ejercicio de ejemploExpresionesLogicas terminado

--- src/main/java/variables/EjemploExpresionesLogicas.java
+++ src/main/java/variables/EjemploExpresionesLogicas.java
@@ -56,32 +56,44 @@ public class EjemploExpresionesLogicas {
         System.out.println("Introduce la estatura en cm");
         estatura=teclado.nextInt();
         
-        // a: 25 >= edad <=30
-        boolean a= 25 >= edad && edad<=30;
+        // Su edad esta comprendida entre 25 y 30 (ambos inclusive)
+        boolean a= 25 <= edad && edad<=30;
+        System.out.println("Su edad esta comprendida entre 25 y 30 (ambos inclusive)? " + a);
         
-        // b: 50 >peso <=100
-        boolean b= 50< peso && peso >=100;
-           
+        // Su peso esta comprendido entre 50 y 100 (100 inclusive)?
+        boolean b= 50< peso && peso <=100;
+        System.out.println("""
+                           Su peso esta comprendido entre 50 y 100 (100 inclusive)? %b
+                           """.formatted(b));   
+        
         // c: estatura no mide 175
         boolean c= estatura!=175;
+        System.out.println("Su estatura es diferente a 175 cm?" + c);
         
-        // d: 100 >estatura< 180
+        // d: Su estatura está comprenida entre 100 y 180 (ambos no incluidos)
         boolean d= 100 < estatura && estatura < 180;
+        System.out.println("Su estatura está comprenida entre 100 y 180 (ambos no incluidos)? " + d);
         
-        // e: edad +10 < 60
-        boolean e= edad + 10 > 60;
+        // e: Su edad si sumamos 10 es menos que 60
+        boolean e= edad + 10 < 60;
+        System.out.println("Su edad si sumamos 10 es menos que 60 ? " + e);
         
         // f: nombre = Pablo ignorando mayus y min. Usa equalIgnoreCase
         String pablo = "pablo";
         boolean f = nombre.equalsIgnoreCase("Pablo");
+        System.out.println("Su nombre es Pablo? " + f);
         
         // g  apellido contiene 'j'. Usa contains en String
-        
+        boolean g= apellido1.toLowerCase().contains("j");
+        System.out.println("El apellido contiene la j? " + g);
         
         // h apellido termina en dez. Usa endsWith de los Strings
+        boolean h= apellido1.endsWith("dez");
+        System.out.println("El apellido termina en dez? " + h);
+        
+        // i la edad no esta entre 40 y 50
+        boolean i= edad<40 || edad>50;
+        System.out.println("La edad esta no esta comprendida entre 40 y 50? " + i);
                 
-        // la edad no esta entre 40 y 50
-        
-        
     }
 }
