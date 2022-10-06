34442c2038db7040aba4371f03b89e19eb6e2c41
Edacalm990 <edacalm990@g.educaand.es>
2022-10-06 11:17:00 +0200

    comienzo ejercicio 7

---
+++ src/main/java/algoritmos/Ej07.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package algoritmos;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author elisabet
+ */
+
+/*Conversor de Euros a Dolares*/
+public class Ej07 {
+    public static void main(String[] args) {
+        final double DOLARPOREURO = 0.99; 
+        String euros = JOptionPane.showInputDialog("Cuantos euros quieres convertir?");
+    }
+}
